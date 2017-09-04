2f2467e750d8c0edd0421925a3c40eafadef8f8a
willispyoung <willispyoung@gmail.com>
2017-09-05 01:51:29 +0800

    finish init_port.py

--- scripts/init_port.py
+++ scripts/init_port.py
@@ -1,1 +1,37 @@
 # python3
+
+import pymysql
+import socket
+import time
+
+db = pymysql.connect(host="58.205.208.72", port=8779,
+                     user="root", password="thuproxy",
+                     database="thuproxy")
+cur = db.cursor()
+cur.execute("select * from thuproxy_proxyaccount")
+
+for r in cur:
+    user_type = r[2]
+    expire_time = r[4]
+    port = r[5]
+
+    # no valid user
+    if user_type == 0:
+        continue
+
+    expire = int(time.mktime(time.strptime(str(expire_time), "%Y-%m-%d")))
+    now = int(time.time())
+
+    # account already expire
+    if expire < now:
+        continue
+
+    cmd = "addport@" + str(port) + "," + str(user_type)
+    print(cmd)
+    control = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    control.connect(("118.190.151.42", 4127))
+    control.send(bytes(cmd, encoding="utf-8"))
+    control.close()
+
+cur.close()
+db.close()
