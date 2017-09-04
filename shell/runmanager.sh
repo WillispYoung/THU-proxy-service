f7530ff90044039cd8ed9aa7b1d2993c4fa1b482
willispyoung <willispyoung@gmail.com>
2017-09-05 01:05:16 +0800

    add shell

---
+++ shell/runmanager.sh
@@ -0,0 +1,3 @@
+cd /root/THU-proxy-service/src;
+javac proxy/*.java;
+java proxy.ProxyManager >> /root/THU-proxy-service/log/manager.log &
---
+++ shell/runserver.sh
@@ -0,0 +1,3 @@
+cd /root/THU-proxy-service/src;
+javac proxy/*.java;
+java proxy.Server >> /root/THU-proxy-service/log/server.log &
