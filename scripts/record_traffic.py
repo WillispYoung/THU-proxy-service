090f4872e6e34e9c186e4349ba7fd050b5a40698
willispyoung <willispyoung@gmail.com>
2017-09-05 19:57:11 +0800

    update

--- scripts/record_traffic.py
+++ scripts/record_traffic.py
@@ -1,12 +1,12 @@
 # python3
 # flow file format: 
 #    time: m-d,h:m:s  e.g. 3-4,17:30:20
-#    traffic: coutn of bits  e.g. 10000
+#    traffic: count of bytes  e.g. 10000
 
 import os
 
 output = os.popen("iptables -L -v -n -x").read()
 output = output.replace("\n", "\r\n")
-f = open("/root/THU-proxy-service/log/traffic.log")
+f = open("/root/THU-proxy-service/log/traffic.log", "w")
 f.write(output)
 f.close()
