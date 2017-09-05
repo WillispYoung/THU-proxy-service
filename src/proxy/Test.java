a522515eca5d43e8dec1851440130f7a7d0857da
willispyoung <willispyoung@gmail.com>
2017-09-05 16:54:11 +0800

    update

--- src/proxy/Test.java
+++ src/proxy/Test.java
@@ -21,6 +21,7 @@ public class Test {
 
 	public static void main(String[] args) throws IOException {
 		String ip = args[0];
+		System.out.println(ip);
 		Process process = Runtime.getRuntime().exec("python3 /root/THU-proxy-service/scripts/get_ip_location.py " + ip);
        	BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
         String l;
