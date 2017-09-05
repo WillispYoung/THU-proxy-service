ab80842a12bbdfb87555908a611b7f839dc3d893
willispyoung <willispyoung@gmail.com>
2017-09-05 19:41:46 +0800

    update

--- scripts/record_traffic.py
+++ scripts/record_traffic.py
@@ -3,3 +3,9 @@
 #    time: m-d,h:m:s  e.g. 3-4,17:30:20
 #    traffic: coutn of bits  e.g. 10000
 
+import subprocess
+
+proc = subprocess.Popen("iptables -L -v -n -c", stdout=subprocess.PIPE)
+output = proc.stdout.read()
+
+print output
