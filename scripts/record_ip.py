90a88183807c6e72fac586529aef24b5cf2ee3a8
willispyoung <willispyoung@gmail.com>
2017-09-06 08:14:19 +0800

    update

--- scripts/record_ip.py
+++ scripts/record_ip.py
@@ -6,7 +6,7 @@ port = sys.argv[1]
 ip = sys.argv[2]
 time = sys.argv[3]
 
-f = open("/proxy/ip/" + port + ".ip", "w+")
+f = open("/proxy/ip/" + port + ".ip", "a")
 f.write(time + "\n")
 f.write(ip + "\n")
 f.close()
--- scripts/record_traffic.py
+++ scripts/record_traffic.py
@@ -5,8 +5,25 @@
 
 import os
 
-output = os.popen("iptables -L -v -n -x").read()
-output = output.replace("\n", "\r\n")
-f = open("/root/THU-proxy-service/log/traffic.log", "w")
-f.write(output)
-f.close()
+# output = os.popen("iptables -L -v -n -x").read()
+# output = output.replace("\n", "\r\n")
+# f = open("/root/THU-proxy-service/log/traffic.log", "w")
+# f.write(output)
+# f.close()
+
+output = open("D:/CodeSpace/Python/traffic.log").read()
+lines = output.split("\n")
+record = dict()
+
+i = 0
+count = len(lines)
+while i < count:
+    if lines[i].startswith("Chain"):
+        i += 2
+        while i < count:
+            l = lines[i]
+            if len(l) == 0:
+            	break
+            t = l.split(" ")
+            print(t[1], t[9])
+            i += 1
