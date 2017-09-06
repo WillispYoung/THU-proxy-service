443b2e5ed621079c36e7825a6c689cc218970518
willispyoung <willispyoung@gmail.com>
2017-09-06 19:48:29 +0800

    update

--- scripts/update_traffic.py
+++ scripts/update_traffic.py
@@ -1,27 +1,57 @@
 # python3
+# this script is meant to keep running forever.
 
 import pymysql
-import socket
+import datetime
 import time
+import subprocess
+import glob
+import os
 
-db = pymysql.connect(host="58.205.208.72", port=8779,
-                     user="root", password="thuproxy",
-                     database="thuproxy")
-cur = db.cursor()
-cur.execute("select * from thuproxy_proxyaccount")
+files = glob.glob("/proxy/flow/*.flow")
+record = dict()
 
-for r in cur:
-    user_type = r[2]
-    expire_time = r[4]
-    port = r[5]
-    traffic = r[7]
+for f in files:
+    port = int(f.split("/")[-1].split(".")[0])
+    last_traffic = os.popen("tail -n 1 " + f).read()
+    record[port] = int(last_traffic)
 
-    w = open("/proxy/flow/" + str(port) + "/flow")
+while True:
     now = time.strftime("%m-%d,%H:%M:%S", time.localtime(time.time()))
+    hour = int(now.split(",")[1].split("":)[0])
+    if hour % 2 != 0:
+        time.sleep(1800)
+        continue
 
+    # yesterday = str(datetime.date.today() - datetime.timedelta(days=1))[5:]
+    output = os.popen("iptables -L -v -n -x").read()
+    lines = output.split("\n")
 
+    i = 0
+    p = re.compile(r' +')
+    count = len(lines)
+    while i < count:
+        if lines[i].startswith("Chain"):
+            i += 2
+            while i < count:
+                l = lines[i]
+                if len(l) == 0:
+                    break
+                t = p.split(l)
+                traffic = int(t[2])
+                port = int(t[-1].split(":")[-1])
+                if port in record:
+                    record[port] += traffic
+                else:
+                    record[port] = traffic
+                i += 1
+        i += 1
     
+    for p in record:
+        w = open("/proxy/flow/" + str(p) + ".flow", "a")
+        w.write(now + "\n")
+        w.write(str(record[p]))
+        w.close()
 
-
-cur.close()
-db.close()
+    os.popen("iptables -Z INPUT")
+    os.popen("iptables -Z OUTPUT")
