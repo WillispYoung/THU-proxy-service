e6a3c928c271f427228e9ceb2eaf35f68e946912
willisp <willispyoung@gmail.com>
2017-10-01 00:45:13 +0800

    modify reset-traffic.py

--- scripts/reset_traffic.py
+++ scripts/reset_traffic.py
@@ -1,3 +1,27 @@
-import glob
+import os
+import re
+import pymysql
+import socket
+import time
 
-folder = ""
+db = pymysql.connect(host="58.205.208.72", port=8779,
+                     user="root", password="thuproxy",
+                     database="thuproxy")
+cur = db.cursor()
+cur.execute("select * from thuproxy_proxyaccount")
+
+for r in cur:
+    user_type = r[2]
+    expire_time = r[4]
+    port = r[5]
+
+    w = open("/proxy/flow/" + str(port) + ".flow", "a")
+    now = time.strftime("%m-%d,%H:%M:%S", time.localtime(time.time()))
+    
+    w.write(now + "\n")
+    w.write("0.0\n")
+    w.close()
+
+
+cur.close()
+db.close()
